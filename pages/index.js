401ade1b3af4ec3b92b26e79b6a4e02026a20777
Alex Cosmas <2alexcosmas@gmail.com>
2020-10-22 11:43:10 +0300

    barebone nextjs tailwindcss template

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default () => (
+  <div className="bg-gray-100 min-h-screen">
+
+    <div className="hero mx-auto px-12 max-w-5xl py-10">
+      <h1 className="title text-5xl">Simple NextJs TailwindCSS template</h1>
+      <p className="description">To get started, edit <code>pages/index.js</code> and save to reload.</p>
+
+    </div>
+
+  </div>
+);
